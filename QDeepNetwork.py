6bde240c31de1615a3441c54491d9f5af07befc2
trylobit <nbitxd@gmail.com>
2019-01-08 12:40:32 +0100

    Most up to date version

--- QDeepNetwork.py
+++ QDeepNetwork.py
@@ -12,23 +12,30 @@ from keras.optimizers import Adam
 from DinoGameRunner import FreezingGameRunner
 
 class QNAgent:
-    def __init__(self, input_shape, output_shape=3):
+    def __init__(self, input_shape, output_shape=3, memory_size=2048, error_memory_size=128, max_domination_rate=2):
         self._input_shape = input_shape
         self._output_shape = output_shape
-        self._memory = deque(maxlen=5000)
+        self._memory = deque(maxlen=memory_size)
+        self._error_memory = deque(maxlen=error_memory_size)
         self._gamma = 0.95  # discount rate
-        self._epsilon = 0.2  # exploration rate
+        self._epsilon = 1  # exploration rate
         self._epsilon_min = 0.001
-        self._epsilon_decay = 0.995
-        self._learning_rate = 0.001
+        self._epsilon_decay = 0.95
+        self._exploration_turns = 25
+        self._learning_rate = 1e-3
         self._model = self._build_model()
+
+        self._memory_stats = [0] * self._output_shape
+        self._error_memory_stats = [0] * self._output_shape
+        self._max_domination_rate = max_domination_rate
 
     def _build_model(self):
         model = Sequential()
 
-        model.add(Conv2D(kernel_size=2, filters=4, activation='relu', input_shape=self._input_shape))
-        model.add(Conv2D(kernel_size=3, filters=8, activation='relu'))
+        model.add(Conv2D(kernel_size=3, filters=16, activation='relu', input_shape=self._input_shape))
+        model.add(Conv2D(kernel_size=5, filters=8, activation='relu'))
         model.add(Flatten())
+        model.add(Dense(units=64, activation='relu'))
         model.add(Dense(units=self._output_shape, activation='linear'))
 
         model.compile(loss='mse', optimizer=Adam(lr=self._learning_rate))
@@ -46,16 +53,38 @@ class QNAgent:
         self._model.fit(st, target_f, epochs=1, verbose=0)
 
     def remember(self, state, action, reward, next_state, done):
-        self._memory.append((state, action, reward, next_state, done))
+        if done:
+            if self._error_memory_stats[action] <= self._max_domination_rate * min(self._error_memory_stats):
+                if len(self._error_memory) == self._error_memory.maxlen:
+                    self._error_memory_stats[self._error_memory.popleft()[1]] -= 1
+                self._error_memory.append((state, action, reward, next_state, done))
+                self._error_memory_stats[action] += 1
+        else:
+            if self._memory_stats[action] <= self._max_domination_rate * min(self._memory_stats):
+                if len(self._memory) == self._memory.maxlen:
+                    self._memory_stats[self._memory.popleft()[1]] -= 1
+                self._memory.append((state, action, reward, next_state, done))
+                self._memory_stats[action] += 1
+
+    def explore(self):
+        r = random.randrange(self._output_shape)
+        print('RANDOM', r)
+        return r
 
     def act(self, state, explore=True):
         if explore and np.random.rand() <= self._epsilon:
-            return random.randrange(self._output_shape)
+            return self.explore()
         st = np.reshape(state, (1, state.shape[0], state.shape[1], 1))
         act_values = self._model.predict(st)
+        print(act_values[0], np.argmax(act_values[0]))
         return np.argmax(act_values[0])
 
-    def replay(self, batch_size):
+    def replay(self, batch_size, error_batch_size):
+        if error_batch_size > len(self._error_memory):
+            error_batch_size = len(self._error_memory)
+        error_minibatch = random.sample(self._error_memory, error_batch_size)
+        for state, action, reward, next_state, done in error_minibatch:
+            self._learn(state, action, reward, next_state, done)
         if batch_size > len(self._memory):
             batch_size = len(self._memory)
         minibatch = random.sample(self._memory, batch_size)
@@ -85,14 +114,18 @@ class TrainingSupervisor:
         'key': 'arrow_down'
     }]
 
-    def __init__(self, qnagent, game_runner, sample_delay=0.1, initial_delay=4):
+    def __init__(self, qnagent, game_runner, sample_delay=0.1, initial_delay=4.75,
+                    punish_rate=1):
         self._qnagent = qnagent
         self._game_runner = game_runner
         self._sample_delay = sample_delay  # how often the screenshots will be taken in sec
         self._initial_delay = initial_delay
+        self._punish_rate = punish_rate
         self._scores = []
 
     def _preprocess(self, img):
+        img = img.crop((60, 0, 600, 150))
+        img = img.resize((135, 50))
         ar = np.array(img)
         ar = np.average(ar[:, :, :3], axis=2)
         ar = ar / 255
@@ -103,7 +136,7 @@ class TrainingSupervisor:
         # print(action['desc'])
         return action['key']
 
-    def train(self, batch_size):
+    def train(self, batch_size, error_batch_size, force_explore=False):
         self._game_runner.start(self._initial_delay)
 
         i = 1
@@ -112,22 +145,25 @@ class TrainingSupervisor:
 
         while not self._game_runner.is_crashed():
             new_state = self._preprocess(self._game_runner.take_screenshot())
-            new_score = self._game_runner.get_score()
 
             if previous_action != -1:
-                self._qnagent.remember(previous_state, previous_action, new_score, new_state, False)
+                self._qnagent.remember(previous_state, previous_action, self._game_runner.get_score(), new_state, False)
 
-            action_id = self._qnagent.act(new_state)
+            if force_explore:
+                action_id = self._qnagent.explore()
+            else:
+                action_id = self._qnagent.act(new_state)
 
             previous_state = new_state
             previous_action = action_id
 
-            self._game_runner.press_key(self._key_from_actionid(action_id))
-
-            if i % 2 * batch_size == 0:
-                self._qnagent.replay(batch_size)
+            if not force_explore and i % (2 * batch_size) == 0:
+                self._qnagent.replay(batch_size, error_batch_size)
 
             i += 1
+
+            self._game_runner.press_key(self._key_from_actionid(action_id))
+
             self._game_runner.play(self._sample_delay)
 
 
@@ -136,8 +172,29 @@ class TrainingSupervisor:
 
         self._scores.append(score)
 
-        self._qnagent.remember(previous_state, previous_action, -100, None, True)
-        self._qnagent.replay(batch_size)
+        self._qnagent.remember(previous_state, previous_action, self._punish_rate * score, None, True)
+        if not force_explore:
+            self._qnagent.replay(batch_size, error_batch_size)
+
+        print('\nMemory size:', len(self._qnagent._memory))
+
+        ac = [0, 0, 0]
+        for state, action, reward, next_state, done in self._qnagent._memory:
+            ac[action] += 1
+
+        print('Memory RUN', ac[0], sep='\t')
+        print('Memory JUMP', ac[1], sep='\t')
+        print('Memory DUCK', ac[2], sep='\t')
+        
+        print('\nError memory size:', len(self._qnagent._error_memory))
+
+        ac = [0, 0, 0]
+        for state, action, reward, next_state, done in self._qnagent._error_memory:
+            ac[action] += 1
+
+        print('Error memory RUN', ac[0], sep='\t')
+        print('Error memory JUMP', ac[1], sep='\t')
+        print('Error memory DUCK', ac[2], sep='\t')
 
         return score
 
--- train_network.py
+++ train_network.py
@@ -15,12 +15,14 @@ if __name__ == '__main__':
     parser.add_argument('--training_sessions', '-n', type=int, action='store')
     parser.add_argument('--headless', action='store_true', default=False)
     parser.add_argument('--input_weights', '-i', action='store', required=False)
-    parser.add_argument('--replay_batch_size', '-r', action='store', type=int, default=32)
+    parser.add_argument('--replay_batch_size', '-r', action='store', type=int, default=64)
+    parser.add_argument('--replay_error_batch_size', action='store', type=int, default=8)
+    parser.add_argument('--pure_exploration_turns', '-e', action='store', type=int, default=100)
     parser.add_argument('--game_path', action='store', default=t_rex_runner_url)
 
     args = parser.parse_args()
 
-    qnagent = QNAgent((150, 600, 1))
+    qnagent = QNAgent((50, 135, 1))
 
     if args.input_weights is not None:
         qnagent.load(args.input_weights)
@@ -32,13 +34,16 @@ if __name__ == '__main__':
     print('=== Begin', args.training_sessions, 'sessions of training')
 
     for n in range(args.training_sessions):
-        print('(', n + 1, '/', args.training_sessions, ')', sep='', end='\t')
-        score = training_supervisor.train(args.replay_batch_size)
-        print('Score', score)
+        print('(', n + 1, '/', args.training_sessions, ')', sep='')
+        score = training_supervisor.train(args.replay_batch_size, args.replay_error_batch_size, force_explore=(n < args.pure_exploration_turns))
+        print('\t\t\t\t\tScore', score)
 
     print('=== Training finished')
     print('Top 10 high scores:', training_supervisor.get_high_scores())
     print('Average score:', training_supervisor.get_average_score())
+    print('Last 50 average:', training_supervisor.get_average_score(50))
+    print('Last 100 average', training_supervisor.get_average_score(100))
+    print('Last 200 average:', training_supervisor.get_average_score(200))
 
     print('=== Saving weights to', args.output_weights)
     qnagent.save(args.output_weights)
