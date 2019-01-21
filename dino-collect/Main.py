fd251f19d6e19b72a2edbb4e168e12868338adfa
trylobit <nbitxd@gmail.com>
2019-01-21 12:49:41 +0100

    Added game stoppping after reaching max speed

--- dino-collect/Main.py
+++ dino-collect/Main.py
@@ -9,11 +9,19 @@ if __name__ == "__main__":
     game = Game()
     data_collector = DataCollector(game)
     INTERVAL = 0.03
+    MAX_DISTANCE = 1400  # After this dino is moving with max speed
     try:
         while True:
             if game.is_crashed():
                 game.restart()
-            else:
+                if data_collector.counter > 20000:
+                    raise KeyboardInterrupt
+            elif data_collector.get_dino_distance() > MAX_DISTANCE:
+                game.toggle_bot()
+                while not game.is_crashed():
+                    time.sleep(1.)
+                game.toggle_bot()
+            else:                    
                 data_collector.grab_screen_data()
                 time.sleep(INTERVAL)
     except Exception:
