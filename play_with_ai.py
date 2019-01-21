881b8a06f691523ffe7680b42288cf3bad20c6a9
trylobit <nbitxd@gmail.com>
2019-01-21 12:19:59 +0100

    Added ConvNet4FramePlayer

--- play_with_ai.py
+++ play_with_ai.py
@@ -9,6 +9,7 @@ import cv2
 
 import argparse
 import os
+from collections import deque
 
 def auroc(y_true, y_pred):
     return tf.py_func(roc_auc_score, (y_true, y_pred), tf.double)
@@ -44,6 +45,37 @@ class ConvNetPlayer(object):
             self.runner.press_key("space")
         
 
+class ConvNet4FramePlayer:
+    def __init__(self, model_path, runner):
+        self.model = load_model(model_path, custom_objects={'auroc': auroc})
+        self.runner = runner
+        self.last_distance = 0
+        self.frame_queue = deque()
+
+    def preprocess_im(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2GRAY)
+        image = image[:300, :500]
+        image = cv2.resize(image, (80,80))
+        # cv2.imwrite("test2.png", image)
+        return image
+
+    def perform_action(self, dino_params):
+        if dino_params['distance'] <= self.last_distance:
+            return
+        self.last_distance = dino_params['distance']
+        picture = self.preprocess_im(self.runner.take_screenshot())
+        self.frame_queue.append(picture)
+        if len(self.frame_queue) < 4:
+            return
+        elif len(self.frame_queue) == 5:
+            self.frame_queue.popleft()
+        if dino_params['distance'] < 35:
+            return
+        action = self.model.predict(np.array(self.frame_queue).reshape((1, 80, 80, 4)))[0]
+        print('action', action)
+        if action.argmax() == 1:
+            self.runner.press_key('space')
+
 
 if __name__ == "__main__":
     working_dir = os.path.dirname(os.path.realpath(__file__))
@@ -54,9 +86,13 @@ if __name__ == "__main__":
 
     parser.add_argument("--game_path", "-p", action="store", default=t_rex_runner_url)
     parser.add_argument("--model_path", "-m", action="store", default="with_speeds.h5")
+    parser.add_argument("--four_frames", "-4", action="store_true", default=False)
     args = parser.parse_args()
 
     game_runner = DinoGameRunner(args.game_path)
-    ai = ConvNetPlayer(args.model_path, game_runner)
+    if args.four_frames:
+        ai = ConvNet4FramePlayer(args.model_path, game_runner)
+    else:
+        ai = ConvNetPlayer(args.model_path, game_runner)
     game_runner.run_game(ai=ai)
  
